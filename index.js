502394bc3040f2514bcd519c7c48c52a7191bd8d
richknicks <rlynch@gmail.com>
2019-12-18 17:48:00 -0500

    complete test

--- index.js
+++ index.js
@@ -93,22 +93,35 @@ Car.prototype.drive = function(distance){
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
-    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+    - Besides `name` and `age`, Baby takes a third argument to initialize `childAttributes`.
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
-}
-
+  function Baby(name, age, favoriteToy) {
+    Person.call (this, name, age);
+    this.favoriteToy = favoriteToy;
+  }
+    
+    Baby.prototype = Object.create(Person.prototype);
+    Baby.prototype.play = function(){
+      return `Play with ${this.favoriteToy};`
+    }
+  
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1.Window/Global Object Binding When in the global scope, the value of “this”
+   will be the window/console Object.
+
+  2.Implicit Binding is when a preceding dot calls a function, the object 
+  before the dot is this. This principle is one of the most commonly used 
+  applications of the this keyword.
+
+  3.New binding Whenever we use a constructor function, this refers to the
+   specific instance of the object that is created and returned by the constructor function.
+
+  4.Explicit binding Whenever we use JavaScript’s call or apply method, this is explicitly defined.
 */
 
 
